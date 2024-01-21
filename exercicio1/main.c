e7618a62d5e074bdb341a1eea629069f1b142583
leandro <leandro_antunes28@hotmail.com>
2024-01-21 02:53:50 -0300

    estudos linguagem c

---
+++ exercicio1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+void main()
+{
+    printf("Listagem de Alunos\n");
+    printf("-------------------\n");
+    printf("Nome \t\tNota\n");
+    printf("Ana Beatriz \t8.5\n");
+    printf("Bianca Martins \t9.0\n");
+    printf("Claudio Sa \t5.5\n");
+    printf("Giovana Silva \t7.5\n");
+}
