d596ab716d13b6a777a31bd3031d187d0ddbf940
leandro <leandro_antunes28@hotmail.com>
2024-01-22 03:51:42 -0300

    aprimorando conhecimento na linguagem C

--- bloco8/main.c
+++ bloco8/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
-int main()
+void main()
 {
-    printf("Hello, World!\n");
-    return 0;
+    char nomeAluno[20];
+    float nota1,nota2;
+
+    printf("Digite o nome do aluno: ");
+    fgets(nomeAluno, 20, stdin);
+
+    printf("Digite a primeira nota: ");
+    scanf("%f", &nota1);
+
+    printf("Digite a segunda nota: ");
+    scanf("%f", &nota2);
+
+    printf("Nome do Aluno: %s \n", nomeAluno);
+    printf("A media das notas foi %.2f \n", (nota1 + nota2)/2);
+
+
 }
